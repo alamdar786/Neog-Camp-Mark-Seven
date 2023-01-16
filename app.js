528770c3aaefc4675a7d9e702596bf7acd3a02ac
Alamdar <alamdar.cse@gmail.com>
2023-01-16 18:59:39 +0530

    Input Output Processing done

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ var btnTranslate = document.querySelector("#btn-translate");
 
 var txtInput = document.querySelector("#txt-input");
 
-console.log(txtInput);
+var outputDiv = document.querySelector("#output");
+
+
+
 
 function clickHandler() {
-    console.log("Clicked!");
-    console.log("input", txtInput.value);
+    outputDiv.innerText = "ajajaja " + txtInput.value;
 }
 
 btnTranslate.addEventListener("click", clickHandler)
