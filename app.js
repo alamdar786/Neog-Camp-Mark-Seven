222901d2d0ce501552b4803083ad7bd151ced14c
Alamdar <alamdar.cse@gmail.com>
2023-01-18 00:48:01 +0530

    Indentations

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 var btnTranslate = document.querySelector("#btn-translate");
-
 var txtInput = document.querySelector("#txt-input");
-
 var outputDiv = document.querySelector("#output");
 
-// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yod.json";
 
 var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
@@ -26,8 +23,7 @@ function clickHandler() {
     .then(response => response.json())
     .then(json => {
         var translatedText = json.contents.translated;
-        outputDiv.innerText = translatedText; //Output
-        
+        outputDiv.innerText = translatedText; //Output    
     })  
     .catch(errorHandler)
 };
