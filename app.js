9bd1017de4a90c8ae3d70be6f7fd754cad6c0e15
Alamdar <alamdar.cse@gmail.com>
2023-01-17 22:28:39 +0530

    Added Fun Translations

--- app.js
+++ app.js
@@ -4,11 +4,32 @@ var txtInput = document.querySelector("#txt-input");
 
 var outputDiv = document.querySelector("#output");
 
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yod.json";
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("Something wrong with Server! Try again after some time");
+}
 
 function clickHandler() {
-    outputDiv.innerText = "ajajaja " + txtInput.value;
-}
+
+    var inputText = txtInput.value; //Taking Input
+
+    //Calling the server for processing
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText; //Output
+        
+    })  
+    .catch(errorHandler)
+};
 
 btnTranslate.addEventListener("click", clickHandler)
